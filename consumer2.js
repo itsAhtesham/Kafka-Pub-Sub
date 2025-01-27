8b3ce7ab06d48330bbafe518889ea85d36443808
itsAhtesham <ahteshamsiddiqui5900@gmail.com>
2025-01-28 02:59:59 +0530

    Added graceful shutdown.

--- consumer2.js
+++ consumer2.js
@@ -22,6 +22,32 @@ const consumer = kafka.consumer({ groupId: 'group' + Math.random() });
   }
 })();
 
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.forEach(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.forEach(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
+
 app.listen(port, () => {
   console.log(`Consumer 2 server running at http://localhost:${port}`);
 });
--- producer.js
+++ producer.js
@@ -39,6 +39,32 @@ app.post('/send-message', async (req, res) => {
     }
 });
 
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.forEach(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect()
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.forEach(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
+
 app.listen(3000, () => {
     console.log("server listening on port 3000");
 })
